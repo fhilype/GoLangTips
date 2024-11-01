0ea58301f934656c4af5d5d6407388469b8ba4ae
Fhilype <fhilype@hotmail.com>
2024-10-31 22:03:44 -0300

    new hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Olá, Mundo!");
+	var nome string = "Fhilype"
+	sobrenome := "Medeiros"
+	var idade int = 31
+	var versao float32 = 1.1
+	fmt.Println("Olá", nome, sobrenome, "seja bem vindo")
+
+	impressao := "Sua idade é "
+	impressao += strconv.Itoa(idade)
+	impressao += " anos"
+	fmt.Println(impressao)
+
+	fmt.Println("Este programa esta na versão: ", versao)
+
+	fmt.Println("Tipo da variável idade:", reflect.TypeOf(idade))
+	fmt.Println("Tipo da variável versao:", reflect.TypeOf(versao))
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir LOGs")
+	fmt.Println("0 - Sair")
+
+	var comando int
+	/*
+		fmt.Scanf
+		%d é o modificador onde a leitura espera um número int como entrada.
+		& indica o endereço. Logo &comando, o endereço da variável comando.
+	*/
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O dado de entrada foi", comando, "e seu endereço é", &comando)
+	/*
+		fmt.Scan (preferível)
+		não recebe um modificador como parametro.
+		& indica o endereço. Logo &comando, o endereço da variável comando.
+	*/
+	fmt.Scan(&comando)
+	fmt.Println("O dado de entrada foi", comando, "e seu endereço é", &comando)
 }
