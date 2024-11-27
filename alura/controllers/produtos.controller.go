0be9e3604400c5364c3f6a586905b2660a6c722d
Fhilype <fhilype@hotmail.com>
2024-11-26 22:58:36 -0300

    edit and update

--- alura/controllers/produtos.controller.go
+++ alura/controllers/produtos.controller.go
@@ -22,18 +22,40 @@ func Insert(responseWriter http.ResponseWriter, request *http.Request) {
 	if request.Method == "POST" {
 		nome := request.FormValue("nome")
 		descricao := request.FormValue("descricao")
-		preco, err := strconv.ParseFloat(request.FormValue("preco"), 64)
+		preco, _err := strconv.ParseFloat(request.FormValue("preco"), 64)
 		quantidade, err := strconv.Atoi(request.FormValue("quantidade"))
-		if err != nil {
+		if err != nil || _err != nil {
 			log.Println("Erro ao converter:", err)
 		}
 		models.CriarNovoProduto(nome, descricao, preco, quantidade)
 	}
-	http.Redirect(responseWriter, request, "/", 301)
+	http.Redirect(responseWriter, request, "/", http.StatusMovedPermanently)
 }
 
 func Delete(responseWriter http.ResponseWriter, request *http.Request) {
 	id := request.URL.Query().Get("id")
 	models.DeletaProduto(id)
-	http.Redirect(responseWriter, request, "/", 301)
+	// http.Redirect(responseWriter, request, "/", 301) // literal
+	http.Redirect(responseWriter, request, "/", http.StatusMovedPermanently)
+}
+
+func Edit(responseWriter http.ResponseWriter, request *http.Request) {
+	id := request.URL.Query().Get("id")
+	produto := models.EditaProduto(id)
+	templates.ExecuteTemplate(responseWriter, "edit", produto)
+}
+
+func Update(responseWriter http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		id, err := strconv.Atoi(request.FormValue("id"))
+		nome := request.FormValue("nome")
+		descricao := request.FormValue("descricao")
+		preco, _err := strconv.ParseFloat(request.FormValue("preco"), 64)
+		quantidade, err_ := strconv.Atoi(request.FormValue("quantidade"))
+		if err != nil || _err != nil || err_ != nil {
+			log.Println("Erro ao converter:", err)
+		}
+		models.AtualizaProduto(id, nome, descricao, preco, quantidade)
+	}
+	http.Redirect(responseWriter, request, "/", http.StatusMovedPermanently)
 }
--- alura/models/produtos.go
+++ alura/models/produtos.go
@@ -55,9 +55,48 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 
 func DeletaProduto(id string) {
 	db := db.ConectaComBancoDeDados()
-	deleteById, err := db.Prepare("DELETE FROM produtos WHERE id=$1")
+	deleteById, err := db.Prepare("DELETE FROM produtos WHERE id=$1 ORDER BY id ASC")
 	if err == nil {
 		deleteById.Exec(id)
+	} else {
+		panic(err.Error())
+	}
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConectaComBancoDeDados()
+	produtoDoBanco, err := db.Query("SELECT * FROM produtos WHERE id=$1", id)
+	produto := Produto{}
+	if err == nil {
+		for produtoDoBanco.Next() {
+			var id, quantidade int
+			var nome, descricao string
+			var preco float64
+
+			err = produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+			if err == nil {
+				produto.Id = id
+				produto.Nome = nome
+				produto.Descricao = descricao
+				produto.Preco = preco
+				produto.Quantidade = quantidade
+			} else {
+				panic(err.Error())
+			}
+		}
+	} else {
+		panic(err.Error())
+	}
+	defer db.Close()
+	return produto
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+	updateValues, err := db.Prepare("UPDATE produtos SET nome=$1, descricao=$2, preco=$3, quantidade=$4 WHERE id=$5")
+	if err == nil {
+		updateValues.Exec(nome, descricao, preco, quantidade, id)
 	} else {
 		panic(err.Error())
 	}
